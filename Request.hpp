50cbc01cce654068dad673bc59a9ee60d9fe6ef3
etaiami09-cmd <etaiami09@gmail.com>
2026-05-23 21:27:58 +0300

    Solidified error handling in the low-level request infrastructure, changed buffer sizes to be dynamic to avoid partial reading and allow for custom buffer sizes

--- Request.hpp
+++ Request.hpp
@@ -2,9 +2,12 @@
 #define HTTP_REQUEST_HPP
 
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string>
+#include <iostream>
+#include <functional>
 
 int sockfd;
 
@@ -12,18 +15,79 @@ constexpr int PORT = 8080;
 constexpr int MAX_CLIENTS = 10;
 constexpr size_t MAX_REQUEST_SIZE = 4096;
 
-void initServer() {
+static inline void handleSocketFail() {
+    std::cerr << "Fatal Error: Failed to register a socket, terminating program\n";
+    exit(EXIT_FAILURE);
+}
+static inline void handleFailedSetSockOpt() {
+    std::cerr << "Fatal Error: Failed to set sock opt, terminating program\n";
+    exit(EXIT_FAILURE);
+}
+static inline void handleFailedBind() {
+    std::cerr << "Fatal Error: Failed to bind, terminating program\n";
+    exit(EXIT_FAILURE);
+}
+static inline void handleFailedListen() {
+    std::cerr << "Fatal Error: listen() failed, terminating program\n";
+    exit(EXIT_FAILURE);
+}
+
+using FailedSocketHandler = std::function<void()>;
+using FailedSetSockOptHandler = std::function<void()>;
+using FailedBindHandler = std::function<void()>;
+using FailedListenHandler = std::function<void()>;
+
+static inline FailedSocketHandler failedSocket = handleSocketFail;
+static inline FailedSetSockOptHandler failedSetSockOpt = handleFailedSetSockOpt;
+static inline FailedBindHandler failedBind = handleFailedBind;
+static inline FailedListenHandler failedListen = handleFailedListen;
+
+inline void initServer() {
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        failedSocket();
+        return;
+    }
     int opt = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        failedSetSockOpt();
+        close(sockfd);
+        return;
+    }
 
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
-    bind(sockfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+    if (bind(sockfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0)
+    {
+        failedBind();
+        close(sockfd);
+        return;
+    }
 
-    listen(sockfd, MAX_CLIENTS);
+    if (listen(sockfd, MAX_CLIENTS) < 1) {
+        failedListen();
+        close(sockfd);
+        return;
+    }
+}
+
+inline void setFailedSocketCallback(FailedSocketHandler socketHandler) {
+    failedSocket = socketHandler;
+}
+
+inline void setFailedSetSockOptCallback(FailedSocketHandler setSockOptHandler) {
+    failedSetSockOpt = failedSetSockOpt;
+}
+
+inline void setFailedBindCallback(FailedBindHandler bindHandler) {
+    failedBind = bindHandler;
+}
+
+inline void setFailedListenHandler(FailedListenHandler listenHandler) {
+    failedListen = listenHandler;
 }
 
 struct Request {
@@ -34,9 +98,22 @@ struct Request {
     }
 
     std::string readRequest() {
-        char buf[MAX_REQUEST_SIZE];
-        ssize_t readSize = read(client, buf, sizeof(buf) - 1);
-        return std::string(buf, readSize);
+        int requestSize;
+        ioctl(client, FIONREAD, &requestSize);
+        char* buffer = new char[requestSize];
+        ssize_t readSize = read(client, buffer, requestSize - 1);
+        std::string result(buffer, readSize);
+        delete[] buffer;
+        return result;
+    }
+
+    std::string readRequest(size_t charAmount) {
+        char* buffer = new char[charAmount];
+        ssize_t readSize = read(client, buffer, charAmount - 1);
+        buffer[charAmount - 1] = 0;
+        std::string result(buffer, readSize);
+        delete[] buffer;
+        return result;
     }
 
     void respond(const std::string& message) {
@@ -46,7 +123,7 @@ struct Request {
     ~Request() {close(client);}
 };
 
-void closeServer() {
+inline void closeServer() {
     close(sockfd);
 }
 
